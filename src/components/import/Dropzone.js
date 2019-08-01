4f0f321fe12f790ab7adeea728f8373b36180bc8
SamLife91 <samir_barguti@hotmail.com>
2019-08-01 15:57:33 +0300

    add IIFE for conditional rendering

--- src/components/import/Dropzone.js
+++ src/components/import/Dropzone.js
@@ -1,8 +1,7 @@
-import React, { useCallback, useState, useEffect, createRef } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import Paper from "@material-ui/core/Paper";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import CheckCircleOutline from "@material-ui/icons/CheckCircleOutline";
 
@@ -16,11 +15,6 @@ const Dropzone = () => {
       console.log("async");
       setUploaded({ isUploaded: true });
     }, 5000);
-  }, []);
-  const onClick = useCallback(acceptedFile => {
-    // file upload
-    open;
-    console.log("clicked");
   }, []);
   const {
     acceptedFiles,
@@ -36,26 +30,22 @@ const Dropzone = () => {
     noClick: false,
     noKeyboard: true
   });
-  console.log(useDropzone);
   return (
     <Paper
       elevation={12}
       style={{
         display: "flex",
+        flexDirection: "column",
         width: "100%",
-        padding: "5px 10px",
-        alignSelf: "center",
-        margin: "0 auto"
+        alignSelf: "center"
       }}
-      maxWidth={false}
     >
       <div
         {...getRootProps()}
         style={{
           borderRadius: 8,
           padding: 0,
-          height: "250px",
-          height: "calc(100% - 250px)",
+          padding: "4.5vh",
           display: "flex",
           flex: 1,
           flexDirection: "column",
@@ -82,44 +72,50 @@ const Dropzone = () => {
           <p style={{ color: "#fff" }}>Drop some files here ...</p>
         )}
       </div>
-      <div
-        style={{
-          display: "flex",
-          flex: 2,
-          justifyContent: "center",
-          flexDirection: "column",
-          padding: "15px 15px"
-        }}
-      >
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div
-            style={{
-              display: "inherit",
-              flexDirection: "row",
-              flex: 1,
-              alignItems: "center"
-            }}
-          >
-            <div style={{ flex: 3, textAlign: "left" }}>File Name</div>
-            <div style={{ flex: 1, alignSelf: "flex-end", textAlign: "right" }}>
-              <CheckCircleOutline
-                style={{ color: isUploaded ? "green" : "grey" }}
-              />
+      {(() => {
+        if (acceptedFiles.length) {
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                padding: "3.5vh"
+              }}
+            >
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div
+                  style={{
+                    display: "inherit",
+                    flexDirection: "row",
+                    flex: 1,
+                    alignItems: "center"
+                  }}
+                >
+                  <div style={{ flex: 3, textAlign: "left" }}>
+                    {acceptedFiles.map(el => el.path)}
+                  </div>
+                  <div
+                    style={{
+                      flex: 1,
+                      alignSelf: "flex-end",
+                      textAlign: "right"
+                    }}
+                  >
+                    <CheckCircleOutline
+                      style={{ color: isUploaded ? "green" : "grey" }}
+                    />
+                  </div>
+                </div>
+                <Divider />
+                <div style={{ flex: 1 }}>
+                  <LinearProgress />
+                </div>
+              </div>
             </div>
-          </div>
-          <Divider />
-          <div style={{ flex: 1 }}>
-            <LinearProgress />
-          </div>
-        </div>
-
-        {acceptedFiles.map(el => el.path)}
-        {isDragAccept && <LinearProgress />}
-
-        {/* <Button type="button" variant="contained" onClick={() => onClick()}>
-          Default
-        </Button> */}
-      </div>
+          );
+        }
+      })()}
     </Paper>
   );
 };
